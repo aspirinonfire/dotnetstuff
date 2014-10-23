8c08b640896adae5b7728afe09bd25b69c0ecca2
aspirinonfire <alex.chernyak@gmail.com>
2014-10-22 17:23:06 -0700

    Added empty authentication controller

---
+++ MVC5Demo/Mvc5Demo/Authentication/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5Demo.Authentication
+{
+  /// <summary>
+  /// Authentication controller
+  /// http://www.khalidabuhakmeh.com/asp-net-mvc-5-authentication-breakdown-part-deux
+  /// http://blogs.msdn.com/b/webdev/archive/2013/07/03/understanding-owin-forms-authentication-in-mvc-5.aspx
+  /// </summary>
+  public class AuthenticationController : Controller
+  {
+    // GET: Authentication
+    public ActionResult Login()
+    {
+      return View();
+    }
+  }
+}
